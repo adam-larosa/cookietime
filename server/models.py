5efc7656f53e976a814de632a874051069f7e019
Adam La Rosa <adam.larosa@flatironschool.com>
2023-04-15 13:39:18 -0700

    SQLAlchemy

---
+++ server/models.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+md = MetaData( naming_convention = {
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+} )
+
+db = SQLAlchemy( metadata = md )
