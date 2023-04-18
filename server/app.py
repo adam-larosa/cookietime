d7d8846617c2ff92b0a009cf6928efc7a5374364
Adam La Rosa <adam.larosa@flatironschool.com>
2023-04-18 07:00:08 -0700

    minor update

--- server/app.py
+++ server/app.py
@@ -9,12 +9,11 @@ class Users( Resource ):
     def get( self ):
         #ipdb.set_trace()
         response = make_response( { 'hello': 'meow' }, 200 )
-        response.set_cookie( 'meow name', 'meow value' )
         return response
 
     def post( self ):
         #ipdb.set_trace()
-        return make_response( { 'hello': session['meow'] }, 200 )
+        return make_response( { 'hello': 'meow' }, 200 )
 
 
 api.add_resource( Users, '/users' )
